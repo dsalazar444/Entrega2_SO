b6084c631a18285f1d37cab6a675c385152259fc
Emmanuel Alvarez Castrillon <ealvarezc1@eafit.edu.co>
2025-09-17 17:21:03 -0500

    agregando_rutas

--- MiPrimerRobot.java
+++ MiPrimerRobot.java
@@ -35,6 +35,253 @@ public class MiPrimerRobot implements Directions {
             }
         }
     }
+
+    // ------------------- CAMINOS ---------------------
+
+        // Zona Azul - Camino Rápido 
+        public static void caminoAzulRapido(TransportRobot r) {
+            for(int i=0; i<23; i++) r.move(); 
+            r.turnLeft();
+            for(int i=0; i<11; i++) r.move(); 
+            
+            while(r.anyBeepersInBeeperBag()) r.putBeeper();
+
+            for(int i=0; i<4; i++) r.move(); 
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<6; i++) r.move();
+ 
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<6; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<2; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<5; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+        }
+        // Zona Verde - Camino Rápido 
+        public static void caminoVerdeRapido(TransportRobot r) {
+            for(int i=0; i<2; i++) r.move(); 
+            r.turnLeft();
+            for(int i=0; i<7; i++) r.move(); 
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<5; i++) r.move(); 
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move(); 
+            r.turnLeft();
+            for(int i=0; i<4; i++) r.move(); 
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<3; i++) r.move(); 
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move(); 
+            r.turnLeft();
+            for(int i=0; i<5; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<5; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<7; i++) r.move();
+            
+            while(r.anyBeepersInBeeperBag()) r.putBeeper();
+
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<2; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<7; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<6; i++) r.move();
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<6; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<6; i++) r.move();
+        }
+
+        // Zona Azul - Camino Largo 
+        public static void caminoAzulLargo(TransportRobot r) {
+            for(int i=0; i<5; i++) r.move(); 
+            r.turnLeft(); 
+            for(int i=0; i<10; i++) r.move(); 
+            r.turnLeft();
+            for(int i=0; i<3; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<3; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<8; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<4; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<3; i++) r.move();
+            r.turnLeft();
+            
+            for(int i=0; i<5; i++) r.move();
+            
+            r.turnLeft();
+            for(int i=0; i<7; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<5; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<10; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<2; i++) r.move();
+            while(r.anyBeepersInBeeperBag()) r.putBeeper();
+
+            for(int i=0; i<4; i++) r.move(); 
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<6; i++) r.move();
+ 
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<6; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<2; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<5; i++) r.move();
+            r.turnLeft();
+            for(int i=0; i<1; i++) r.move();
+        }
+        // Zona Verde - Camino Largo 
+        public static void caminoVerdeLargo(TransportRobot r) {
+            r.move();
+            r.move();
+            r.turnLeft(); 
+            r.turnLeft(); 
+            r.turnLeft();
+            
+
+            for (int i=0; i<3; i++) r.move();
+
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<8; i++) r.move();
+
+            r.turnLeft();
+            for (int i=0; i<2; i++) r.move();
+
+            r.turnLeft();
+            for (int i=0; i<4; i++) r.move();
+
+            r.turnLeft(); 
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<17; i++) r.move();
+
+            r.turnLeft();
+            for (int i=0; i<5; i++) r.move();
+            
+            r.turnLeft();
+            for (int i=0; i<9; i++) r.move();
+            r.turnLeft(); 
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<8; i++) r.move();
+
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<1; i++) r.move();
+            
+
+            while(r.anyBeepersInBeeperBag()) r.putBeeper();
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<2; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<7; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<6; i++) r.move();
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            r.turnLeft(); 
+            r.turnLeft();
+            for (int i=0; i<6; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<1; i++) r.move();
+            r.turnLeft();
+            for (int i=0; i<6; i++) r.move();
+
+    }
+
 }
 
 // CLASES ADICIONALES
