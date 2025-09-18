133f7320590154b800bc8be264ece03cdd3f0e9a
Emmanuel Alvarez Castrillon <ealvarezc1@eafit.edu.co>
2025-09-18 15:46:50 -0500

    NewFeatures

--- MiPrimerRobot.java
+++ MiPrimerRobot.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Semaphore;
 
 public class MiPrimerRobot implements Directions {
     public static void main(String[] args) {
+        World.setDelay(10);
         World.readWorld("Mundo2.kwld");
         World.setVisible(true);
 
@@ -22,7 +23,7 @@ public class MiPrimerRobot implements Directions {
             }
         }
 
-        for (int i = 0; i < 28; i++) {
+        /*for (int i = 0; i < 28; i++) {
             TransportRobot r = new TransportRobot(i+28, streetGreen, avenueGreen, West, 0, Color.green);
             new Thread(r).start();
             avenueGreen--;
@@ -33,7 +34,7 @@ public class MiPrimerRobot implements Directions {
             if (streetGreen == 12 && avenueGreen < 28){
                 avenueGreen = 23;
             }
-        }
+        }*/
     }
 
     // ------------------- CAMINOS ---------------------
@@ -144,7 +145,7 @@ public class MiPrimerRobot implements Directions {
 
         // Zona Azul - Camino Largo 
         public static void caminoAzulLargo(TransportRobot r) {
-            for(int i=0; i<5; i++) r.move(); 
+            for(int i=0; i<4; i++) r.move(); 
             r.turnLeft(); 
             for(int i=0; i<10; i++) r.move(); 
             r.turnLeft();
@@ -215,16 +216,14 @@ public class MiPrimerRobot implements Directions {
         // Zona Verde - Camino Largo 
         public static void caminoVerdeLargo(TransportRobot r) {
             r.move();
-            r.move();
             r.turnLeft(); 
             r.turnLeft(); 
             r.turnLeft();
             
 
             for (int i=0; i<3; i++) r.move();
-
-            r.turnLeft();
             r.turnLeft(); 
+            r.turnLeft();
             r.turnLeft();
             for (int i=0; i<8; i++) r.move();
 
@@ -297,13 +296,45 @@ class TransportRobot extends Robot implements Runnable {
         World.setupThread(this);
     }
 
+     public static boolean estaEnPosicion(KJRTest Posicion,UrRobot robot, int calle, int avenida) {
+        try {
+            Posicion.assertOnStreet(robot,calle);   // Falla si no está en esa calle
+            Posicion.assertOnAvenue(robot,avenida); // Falla si no está en esa avenida
+            return true; // Si no falló, está en esa posición
+            } catch (AssertionError e) {
+            return false; // Si alguna falla, no está en esa posición
+            }
+    }
+    private void elegirCaminoSegunPosicion() {
+        if (nextToABeeper()) {
+            java.util.Random rand = new java.util.Random();
+            KJRTest Posicion = new KJRTest();
+            if (estaEnPosicion(Posicion, this, 1, 7)) {
+                if (rand.nextBoolean()) {
+                    MiPrimerRobot.caminoAzulRapido(this);
+                } else {
+                    MiPrimerRobot.caminoAzulLargo(this);
+                }
+            } else if (estaEnPosicion(Posicion, this, 12, 23)) {
+                if (rand.nextBoolean()) {
+                    MiPrimerRobot.caminoVerdeRapido(this);
+                } else {
+                    MiPrimerRobot.caminoVerdeLargo(this);
+                }
+            }
+        }
+    }
+
     @Override
     public void run() {
         while (true) {
             if (anyBeepersInBeeperBag() || loadPassengers()) {
-                moveToDestination();
-                unloadPassengers();
-                moveBackToOrigin();
+                elegirCaminoSegunPosicion();
+                int count = 0;
+                while (count < 4) {
+                    pickBeeper();
+                    count++;
+                }
             } else {
                 turnOff();
                 break;
